6cafa87136c45af77daf540456dcd990ac4d622c
Surendra <suren@surendradangol.com.np>
2015-01-22 19:29:05 +0100

    Add consumeStock / produceStock

--- src/com/daubajee/marketmatrix/agent/MarketAgentAttribute.java
+++ src/com/daubajee/marketmatrix/agent/MarketAgentAttribute.java
@@ -8,8 +8,10 @@ public class MarketAgentAttribute {
 	private String consumes = "";
 	private double produceRate = 0;
 	private double consumeRate = 0;
-	private int stock = 0;
-	private int stockMaximum = 0;
+	private int produceProductStock = 0;
+	private int produceProductStockCapacity = 0;
+	private int consumeProductStock = 0;
+	private int consumeProductStockCapacity = 0;
 	private int satisfaction = 0;
 	private double money = 0;
 	private double price = 0;
@@ -17,57 +19,91 @@ public class MarketAgentAttribute {
 	public String getProduces() {
 		return produces;
 	}
+
 	public void setProduces(String produces) {
 		this.produces = produces;
 	}
+
 	public String getConsumes() {
 		return consumes;
 	}
+
 	public void setConsumes(String consumes) {
 		this.consumes = consumes;
 	}
+
 	public double getProduceRate() {
 		return produceRate;
 	}
+
 	public void setProduceRate(double produceRate) {
 		this.produceRate = produceRate;
 	}
+
 	public double getConsumeRate() {
 		return consumeRate;
 	}
+
 	public void setConsumeRate(double consumeRate) {
 		this.consumeRate = consumeRate;
 	}
-	public int getStock() {
-		return stock;
+
+	public int getProduceProductStock() {
+		return produceProductStock;
 	}
-	public void setStock(int stock) {
-		this.stock = stock;
+
+	public void setProduceProductStock(int produceProductStock) {
+		this.produceProductStock = produceProductStock;
 	}
-	public int getStockMaximum() {
-		return stockMaximum;
+
+	public int getProduceProductStockCapacity() {
+		return produceProductStockCapacity;
 	}
-	public void setStockMaximum(int stockMaximum) {
-		this.stockMaximum = stockMaximum;
+
+	public void setProduceProductStockCapacity(int produceProductStockCapacity) {
+		this.produceProductStockCapacity = produceProductStockCapacity;
 	}
+
+	public int getConsumeProductStock() {
+		return consumeProductStock;
+	}
+
+	public void setConsumeProductStock(int consumeProductStock) {
+		this.consumeProductStock = consumeProductStock;
+	}
+
+	public int getConsumeProductStockCapacity() {
+		return consumeProductStockCapacity;
+	}
+
+	public void setConsumeProductStockCapacity(int consumeProductStockCapacity) {
+		this.consumeProductStockCapacity = consumeProductStockCapacity;
+	}
+
 	public int getSatisfaction() {
 		return satisfaction;
 	}
+
 	public void setSatisfaction(int satisfaction) {
 		this.satisfaction = satisfaction;
 	}
+
 	public double getMoney() {
 		return money;
 	}
+
 	public void setMoney(double money) {
 		this.money = money;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public boolean initialize(Map<String, String> attrMap) {
 		
 		for(String key: attrMap.keySet()){
@@ -82,21 +118,25 @@ public class MarketAgentAttribute {
 				consumes = value;
 			} else if (key.equalsIgnoreCase("consumeRate")){
 				consumeRate = Double.parseDouble(value);
-			} else if (key.equalsIgnoreCase("stock")){
-				stock = Integer.parseInt(value);
-			}else if (key.equalsIgnoreCase("stockMaximum")){
-				stockMaximum = Integer.parseInt(value);
-			}else if (key.equalsIgnoreCase("satisfaction")){
+			} else if (key.equalsIgnoreCase("produceStock")){
+				produceProductStock = Integer.parseInt(value);
+			} else if (key.equalsIgnoreCase("produceStockCapacity")){
+				produceProductStockCapacity = Integer.parseInt(value);
+			} else if (key.equalsIgnoreCase("consumeStock")){
+				consumeProductStock = Integer.parseInt(value);
+			} else if (key.equalsIgnoreCase("consumeStockCapacity")){
+				consumeProductStockCapacity = Integer.parseInt(value);
+			} else if (key.equalsIgnoreCase("satisfaction")){
 				satisfaction = Integer.parseInt(value);
-			}else if (key.equalsIgnoreCase("money")){
+			} else if (key.equalsIgnoreCase("money")){
 				money = Double.parseDouble(value);
-			}else if (key.equalsIgnoreCase("price")){
+			} else if (key.equalsIgnoreCase("price")){
 				price = Double.parseDouble(value);
 			}		
 		
 		}			
 		return !(produces.isEmpty() || produceRate ==0|| consumes.isEmpty()
-				|| consumeRate == 0 || stockMaximum == 0 || price == 0);
+				|| consumeRate == 0 || produceProductStockCapacity == 0 || price == 0);
 		
 
 	}
--- src/com/daubajee/marketmatrix/gui/AgentGUI.java
+++ src/com/daubajee/marketmatrix/gui/AgentGUI.java
@@ -1,7 +1,6 @@
 package com.daubajee.marketmatrix.gui;
 
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
@@ -11,27 +10,26 @@ import com.daubajee.marketmatrix.agent.MarketAgentAttribute;
 public class AgentGUI extends Pane {
 
 	private Label agentNameLabel = new Label();
-	private Label producesLabel = new Label();
 	private Label consumesLabel = new Label();
-	private Label produceRateLabel = new Label();
-	private Label consumesRateLabel = new Label();
-	private Label stockLabel = new Label();
-	private Label stockMaximumLabel = new Label();
+	private Label consumeStockLabel = new Label();
+	private Label producesLabel = new Label();
+	private Label priceProductLabel = new Label();
+	private Label produceStockLabel = new Label();
 	private Label satisfactionLabel = new Label();
 	private Label moneyLabel = new Label();
-	private Label priceProductLabel = new Label();
 
 	private String produces = "Wheat";
 	private String consumes = "Rice";
 	private String produceRate = "4";
 	private String consumesRate = "3";
-	private String stock = "10";
-	private String stockMaximum = "100";
+	private String produceStock = "10";
+	private String produceStockMaximum = "100";
+	private String consumeStock = "10";
+	private String consumeStockMaximum = "100";
 	private String satisfaction = "50";
 	private String money = "10";
 	private String price = "3";
 	
-	private HBox titleBox = new HBox();
 	private VBox rootLayout = new VBox();
 	
 	private MarketAgentAttribute attr = null;
@@ -42,14 +40,12 @@ public class AgentGUI extends Pane {
 	public AgentGUI(String name){
 		this.agentName = name;
 		initLayout();
-		//updateLabels();
 	}
 	
 	public void initLayout(){
 		rootLayout.getChildren().addAll(agentNameLabel,
-				consumesLabel, consumesRateLabel,
-				producesLabel, produceRateLabel,
-				priceProductLabel, stockLabel, stockMaximumLabel, 
+				consumesLabel, consumeStockLabel,
+				producesLabel, produceStockLabel, priceProductLabel, 
 				moneyLabel, satisfactionLabel);
 
 		rootLayout.setStyle("-fx-pref-width: 250px; "
@@ -74,20 +70,18 @@ public class AgentGUI extends Pane {
 		produces = attr.getProduces();
 		produceRate = String.valueOf(attr.getProduceRate());
 		price = String.valueOf(attr.getPrice());
-		stock = String.valueOf(attr.getStock());
-		stockMaximum = String.valueOf(attr.getStockMaximum());
+		produceStock = String.valueOf(attr.getProduceProductStock());
+		produceStockMaximum = String.valueOf(attr.getProduceProductStockCapacity());
 		money = String.valueOf(attr.getMoney());
 		satisfaction = String.valueOf(attr.getSatisfaction());
 		
-		consumesLabel.setText("Consumes: " + consumes);
-		consumesRateLabel.setText("Consumption Rate: " + consumesRate + " / day");
-		producesLabel.setText("Produces: " + produces);
-		produceRateLabel.setText("Production Rate: "+ produceRate + " / day");
+		consumesLabel.setText("Consumes: " + consumes + " - " + consumesRate+ " / TU");
+		consumeStockLabel.setText("Consume Stock: " + consumeStock+" / " + consumeStockMaximum);
+		producesLabel.setText("Produces: " + produces + " - " + produceRate + " / TU");
+		produceStockLabel.setText("Produce Stock: " + produceStock+" / " + produceStockMaximum);
 		priceProductLabel.setText("Price of product: "+ price +" MU");
-		stockLabel.setText("Product in stock: "+stock+" units");
-		stockMaximumLabel.setText("Max stock capacity: "+ stockMaximum + " units");
-		moneyLabel.setText("Money : "+money);
-		satisfactionLabel.setText("Satisfaction: "+satisfaction);
+		moneyLabel.setText("Money : "+money + " MU");
+		satisfactionLabel.setText("Satisfaction: "+ satisfaction + "%");
 	}
 	
 	public void setAgentAttribute(MarketAgentAttribute attr){
@@ -121,11 +115,11 @@ public class AgentGUI extends Pane {
 	}
 
 	public void setStock(String stock) {
-		this.stock = stock;
+		this.produceStock = stock;
 	}
 
 	public void setStockMaximum(String stockMaximum) {
-		this.stockMaximum = stockMaximum;
+		this.produceStockMaximum = stockMaximum;
 	}
 
 	public void setSatisfaction(String satisfaction) {
