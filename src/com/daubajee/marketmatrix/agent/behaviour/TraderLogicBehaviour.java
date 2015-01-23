cf9dd3b528072b672d87314e7fd18275889757a9
knil-sama <cols.knil@gmail.com>
2015-01-23 14:26:11 +0100

    implementation of logic for raising or lowering price

--- src/com/daubajee/marketmatrix/agent/behaviour/TraderLogicBehaviour.java
+++ src/com/daubajee/marketmatrix/agent/behaviour/TraderLogicBehaviour.java
@@ -5,19 +5,48 @@ import com.daubajee.marketmatrix.agent.MarketAgent;
 import jade.core.behaviours.Behaviour;
 
 public class TraderLogicBehaviour extends Behaviour {
-
+	private MarketAgent marketAgent;
+	private final double MINIMAL_PRICE = 0.50;
+	private final double MARGE = 0.50;
 	public TraderLogicBehaviour(MarketAgent marketAgent) {
 		marketAgent.printMsg(getClass().getSimpleName() + " initialised");
+		this.marketAgent = marketAgent;
 	}
 
 	@Override
 	public void action() {
-
+		double money = marketAgent.getAttribute().getMoney();
+		double satisfaction = marketAgent.getAttribute().getSatisfaction();
+		if(money > 0 && satisfaction > 0.0){
+			raisePrice();
+		}else if(money <= 0 && satisfaction <= 0.0){
+			dropPrice();
+		}
 	}
 
 	@Override
 	public boolean done() {
-		return true;
+		return false;
 	}
 
+	private void dropPrice() {
+		double actual_price = marketAgent.getAttribute().getPrice();
+		double new_price  = floorPrice(actual_price+MARGE + generateRandomMarge());
+		marketAgent.getAttribute().setPrice(new_price);
+	}
+	private void raisePrice() {
+		double actual_price = marketAgent.getAttribute().getPrice();
+		double new_price  = floorPrice(actual_price+MARGE + generateRandomMarge());
+		marketAgent.getAttribute().setPrice(new_price);
+	}
+	private double floorPrice(double new_price){
+		if(new_price < MINIMAL_PRICE){
+			return MINIMAL_PRICE;
+		}else{
+			return new_price;
+		}
+	}
+	private double generateRandomMarge(){
+		return Math.random() % MARGE;
+	}
 }
