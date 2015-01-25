4e3ada652e284b12c8ed83fdff8091729c97a9a4
Surendra <suren@surendradangol.com.np>
2015-01-25 23:05:52 +0100

    Add agent creation form

--- src/com/daubajee/marketmatrix/gui/Container.java
+++ src/com/daubajee/marketmatrix/gui/Container.java
@@ -1,27 +1,54 @@
 package com.daubajee.marketmatrix.gui;
 
+import com.daubajee.marketmatrix.agent.MarketAgentCreator;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class Container extends BorderPane	{
 
 	private HBox titleContainer = new HBox();
+	private VBox leftcontainer = new VBox();
+	private FlowPane agentCreationBox = new FlowPane();
 	private ScrollPane sp1 = new ScrollPane();
 	private ScrollPane sp2 = new ScrollPane();	
 	private FlowPane agentContainer = new FlowPane();
 	
 	private final String appName = "Market Matrix";
 	private Label title = new Label(appName);
+	
+	private ChoiceBox<String> consumeBox = new ChoiceBox<String>();
+	private ChoiceBox<String> producesBox = new ChoiceBox<String>();
+	Label createAgentLabel = new Label("Create Agent");
+	Label consumeLabel = new Label("Consumes");
+	Label consumeRateLabel = new Label("Rate: ");
+	TextField consumeRateField = new TextField("1");
+	Label produceLabel = new Label("Produces");
+	Label produceRateLabel = new Label("Rate");
+	TextField produceRateField = new TextField("1");
+	Button createBtn = new Button("Create");
+	
 
+	
 	private TextFlow textflow = new TextFlow();
 	
 	public Container(){
@@ -34,12 +61,33 @@ public class Container extends BorderPane	{
 
 		sp2.setContent(textflow);
 		setRight(sp2);
-		setCenter(sp1);
+		setCenter(leftcontainer);
 		
 		sp1.setContent(agentContainer);
 		
 		titleContainer.getChildren().add(title);		
+		leftcontainer.getChildren().addAll(agentCreationBox, sp1);
+		
+		agentCreationBox.setHgap(10);
+		
+		Separator separator = new Separator();
+		separator.setOrientation(Orientation.VERTICAL);
+		Separator separator2 = new Separator();
+		separator2.setOrientation(Orientation.VERTICAL);
+		agentCreationBox.getChildren().addAll(createAgentLabel, 
+				consumeLabel, consumeBox, consumeRateLabel, consumeRateField, 
+				separator,
+				produceLabel, producesBox, produceRateLabel, produceRateField,
+				separator2, createBtn
+				);
+		
+		ObservableList<String> list = FXCollections.observableArrayList();
+		consumeBox.setItems(list);
+		producesBox.setItems(list);
+		list.addAll("Apple", "Banna", "Orange");
 
+		
+		
 /*		for(int i=0; i<10; i++){
 			agentContainer.getChildren().add(new AgentGUI(String.valueOf(i)));
 		}*/
@@ -78,6 +126,8 @@ public class Container extends BorderPane	{
 					+ "-fx-font-family: 'sans-serif';"
 					+ "-fx-font-size: 2.5em; ");
 
+		leftcontainer.setStyle("-fx-min-height: 800px;");
+		
 		sp1.setStyle("-fx-min-height: 500px;"
 				+ "-fx-min-width: 800px;"
 				+ "-fx-padding: 10px;");
@@ -94,7 +144,50 @@ public class Container extends BorderPane	{
 		textflow.setStyle("-fx-pref-width: 400px;");
 		textflow.setLineSpacing(5);
 		
+		
+		createAgentLabel.setStyle("-fx-font-size: 1.8em;");
+		consumeRateField.setStyle("-fx-max-width: 30px;");
+		produceRateField.setStyle("-fx-max-width: 30px;");
+		producesBox.setStyle("-fx-min-width: 100px;");
+		consumeBox.setStyle("-fx-min-width: 100px;");
+		consumeLabel.setStyle("-fx-font-size: 1.0em;"
+				+ "-fx-font-weight: bold;");
+		produceLabel.setStyle("-fx-font-size: 1.0em;"
+				+ "-fx-font-weight: bold;");
+		createBtn.setStyle("-fx-font-size: 1.3em;");
+		
+		createBtn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				String consumes = consumeBox.getSelectionModel().getSelectedItem();
+				String produces = producesBox.getSelectionModel().getSelectedItem();
+				
+				if (produces.equals(consumes)){
+					addMsg("Agents cannot consume and produce same product");
+					return;
+				}
+				
+				try {
+					String consumeRateStr = consumeRateField.getText();
+					String produceRateStr = produceRateField.getText();
+					
+					double cosumeRate = Double.parseDouble(consumeRateStr);
+					double produceRate = Double.parseDouble(produceRateStr);
+				
+				} catch (NumberFormatException e) {
+					addMsg("Please check your data");
+					e.printStackTrace();
+					return;
+				}
+				
+				
+				
+			}
+		});
 	}
+	
+
 	
 	
 }
